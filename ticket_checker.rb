323ca331615cec0a69a65af774c80808df7725a5
Dinesh S Y <50043613+dineshsy@users.noreply.github.com>
2025-09-03 11:37:24 +0530

    Update ticket_checker.rb

--- ticket_checker.rb
+++ ticket_checker.rb
@@ -19,7 +19,16 @@ Mail.defaults do
 end
 
 def tickets_live?
-  response = HTTParty.get(BOOKMYSHOW_URL, follow_redirects: true)
+response = HTTParty.get(
+  BOOKMYSHOW_URL,
+  headers: {
+    "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "\
+                    "(KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+    "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language" => "en-US,en;q=0.9",
+    "Connection" => "keep-alive"
+  }
+)
   p response
   doc = Nokogiri::HTML5(response.body)
  p doc
