37adc79b6b68fa4b3ceb5f9a5be668f9ac5dabcc
Dinesh S Y <50043613+dineshsy@users.noreply.github.com>
2025-09-03 10:44:54 +0530

    Create gmail_test.rb

---
+++ gmail_test.rb
@@ -0,0 +1,19 @@
+require 'mail'
+
+Mail.defaults do
+  delivery_method :smtp, {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV['GMAIL_USER'],
+    password: ENV['GMAIL_PASS'],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+end
+
+Mail.deliver do
+  to "your_email@example.com"
+  from ENV['GMAIL_USER']
+  subject "✅ Gmail SMTP Test"
+  body "If you see this, Gmail App Password setup works!"
+end
